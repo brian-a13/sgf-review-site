6d20441899bd21e64fcb3eab14e379c7661cfc46
brian-a13 <bspam278@gmail.com>
2025-03-03 23:48:14 -0800

    Able to retreive form submissions

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
 const app = express();
+const PORT = 3000;      
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Import routes
+const reviewRoutes = require('./routes/reviews');
+app.use('/reviews', reviewRoutes); // All routes under /reviews
+
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
-
-
-
-app.listen(3000);
 
